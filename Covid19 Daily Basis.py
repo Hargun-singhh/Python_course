f8fc801ac76714234e7a91cfba050d1934791aba
Hargun-singhh <66933848+Hargun-singhh@users.noreply.github.com>
2020-08-03 20:45:24 +0530

    LAST PROJECT :)

---
+++ Covid19 Daily Basis.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+url = "https://api.covid19india.org/data.json"
+
+response = requests.get(url)
+covid_data = json.loads(response.text)
+
+covid_cases_cases= []
+for i in range(0, len(covid_data["cases_time_series"])):
+    covid_cases_cases.append(
+        {
+            "date": covid_data["cases_time_series"][i]["date"],
+            "totalconfirmed": covid_data["cases_time_series"][i]["totalconfirmed"],
+            "totaldeceased": covid_data["cases_time_series"][i]["totaldeceased"],
+            "totalrecovered": covid_data["cases_time_series"][i]["totalrecovered"],
+        }
+    )
+filter1 = input("Please Enter 1st Filter from [date | totalconfirmed | totaldeceased | totalrecovered ]: ")
+filter2 = input("Please Enter 2nd Filter from [date | totalconfirmed | totaldeceased | totalrecovered ]: ")
+filter3 = input("Please Enter 2nd Filter from [date | totalconfirmed | totaldeceased | totalrecovered ]: ")
+
+for i in range(0, len(covid_cases_cases)):
+    print("~~+~~++~~+~~++~~+~~++~~+~~++~~")
+    print("{}: {}\n{}: {}\n{}: {}".format(filter1.upper(), covid_cases_cases[i][filter1], filter2.upper(),
+                                          covid_cases_cases[i][filter2],
+                                          filter3.upper(), covid_cases_cases[i][filter3]))
+    print("~~+~~++~~+~~++~~+~~++~~+~~++~~")
+    print()
+
+
+file = open("COVID19_DATA_DAILY_BASIS.csv", "a")
+file.write(str(covid_data['cases_time_series']))
+print("Data Saved according to day wise :)")
--- Covid19 USER.py
+++ Covid19 USER.py
@@ -8,26 +8,28 @@ covid_data = json.loads(response.text)
 
 covid_cases_india = []
 for i in range(0, len(covid_data["statewise"])):
-
     covid_cases_india.append(
         {
             "state": covid_data["statewise"][i]["state"],
             "active": covid_data["statewise"][i]["active"],
             "confirmed": covid_data["statewise"][i]["confirmed"],
             "deaths": covid_data["statewise"][i]["deaths"],
-            "lastupdatedtime": covid_data["statewise"][i]["deaths"],
         }
     )
 
-filter1 = input("Please Enter 1st Filter from [active | confirmed | deaths | recovered | state]: ")
-filter2 = input("Please Enter 2nd Filter from [active | confirmed | deaths | recovered | state]: ")
-filter3 = input("Please Enter 2nd Filter from [active | confirmed | deaths | recovered | state]: ")
+option1 = input("Please Enter 1st Filter from [active | confirmed | deaths | recovered | state]: ")
+option2 = input("Please Enter 2nd Filter from [active | confirmed | deaths | recovered | state]: ")
+option3 = input("Please Enter 2nd Filter from [active | confirmed | deaths | recovered | state]: ")
 
 for i in range(0, len(covid_cases_india)):
-    print("~~+~~++~~+~~++~~+~~++~~+~~++~~")
-    print("{}: {}\n{}: {}\n{}: {}".format(filter1.upper(), covid_cases_india[i][filter1], filter2.upper(),
-                                          covid_cases_india[i][filter2],
-                                          filter3.upper(), covid_cases_india[i][filter3]))
+    print("_--__--__--__--__--__--__--__--_")
+    print("{}: {}\n{}: {}\n{}: {}".format(option1.upper(), covid_cases_india[i][option1], option2.upper(),
+                                          covid_cases_india[i][option2],
+                                          option3.upper(), covid_cases_india[i][option3]))
     print(covid_data["statewise"][i]["lastupdatedtime"], )
-    print("~~+~~++~~+~~++~~+~~++~~+~~++~~")
+    print("_--__--__--__--__--__--__--__--_")
     print()
+
+file = open("COVID19_DATA_STATE VISE.csv", "a")
+file.write(str(covid_data["statewise"]))
+print("Data Saved :)")
