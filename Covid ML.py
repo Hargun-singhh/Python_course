89ad15dbbdb9eaab072a0bcd8aa0ff3dd26ab201
Hargun-singhh <66933848+Hargun-singhh@users.noreply.github.com>
2020-08-07 13:56:33 +0530

    Final  PROJECT ML :)

---
+++ Covid ML.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("daily.csv")
+
+X = table.Recovered.values
+Y = table.Confirmed.values
+
+plt.scatter(X, Y)
+plt.show()
+# Reshaping to 2D Array
+X = X.reshape(len(X), 1)
+Y = Y.reshape(len(Y), 1)
+
+
+model = LinearRegression()
